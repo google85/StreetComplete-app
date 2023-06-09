c73935ff62dbea7f96da940fe39cb20ca1eb34f3
DjBpF <bpfcomp2005@gmail.com>
2023-06-09 22:52:19 +0300

    sss

--- .patch/prebuild-only-rename.sh
+++ .patch/prebuild-only-rename.sh
@@ -28,7 +28,7 @@ app_dir="$android_dir/app"
 #stubs_dir="$android_dir/.patch/stubs"
 
 echo "APP DIR: $app_dir"
-ls -al $app_dir
+ls -al $android_dir
 
 # BUILD: Add enough memory for the build on FDroid
 #echo -e "\norg.gradle.jvmargs=-XX:MaxHeapSize=4096m" \
